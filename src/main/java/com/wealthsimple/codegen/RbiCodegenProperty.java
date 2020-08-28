1f4fef58d689a26cf95b159ce7da3864431823f0
Kyrylo Shegeda <kshegeda@wealthsimple.com>
2020-08-27 20:20:48 -0400

    Add custom codegen properties

---
+++ src/main/java/com/wealthsimple/codegen/RbiCodegenProperty.java
@@ -0,0 +1,8 @@
+package com.wealthsimple.codegen;
+
+import io.swagger.codegen.CodegenProperty;
+
+public class RbiCodegenProperty extends CodegenProperty {
+    public String rbiDataType;
+
+}
--- src/main/java/com/wealthsimple/codegen/RubyRbiClientCodegen.java
+++ src/main/java/com/wealthsimple/codegen/RubyRbiClientCodegen.java
@@ -1,11 +1,16 @@
 package com.wealthsimple.codegen;
 
 import io.swagger.codegen.languages.*;
+import io.swagger.codegen.CodegenModelFactory;
+import io.swagger.codegen.CodegenModelType;
+import io.swagger.codegen.CodegenProperty;
 import io.swagger.codegen.SupportingFile;
 import io.swagger.models.properties.*;
 
 import java.util.*;
 import java.io.File;
+
+import com.wealthsimple.codegen.RbiCodegenProperty;
 
 public class RubyRbiClientCodegen extends RubyClientCodegen {
   protected String rbiFolder = "rbi";
@@ -17,6 +22,7 @@ public class RubyRbiClientCodegen extends RubyClientCodegen {
     embeddedTemplateDir = templateDir = "RubyRbiClient";
     modelTemplateFiles.put("model_rbi.mustache", ".rbi");
     apiTemplateFiles.put("api_rbi.mustache", ".rbi");
+    CodegenModelFactory.setTypeMapping(CodegenModelType.PROPERTY, RbiCodegenProperty.class);
   }
 
   /**
@@ -67,5 +73,31 @@ public class RubyRbiClientCodegen extends RubyClientCodegen {
     } else {
       return super.apiFilename(templateName, tag);
     }
+  }
+
+  public String getRbiTypeDeclaration(Property p) {
+    if (p instanceof ArrayProperty) {
+      ArrayProperty ap = (ArrayProperty) p;
+      Property inner = ap.getItems();
+      return "T::Array[" + getRbiTypeDeclaration(inner) + "]";
+    } else if (p instanceof MapProperty) {
+      MapProperty mp = (MapProperty) p;
+      Property inner = mp.getAdditionalProperties();
+      return "T::Hash<String, " + getRbiTypeDeclaration(inner) + "]";
+    } else if (p instanceof BooleanProperty) {
+      return "T::Boolean";
+    } else if (p instanceof ObjectProperty) {
+      return "BasicObject";
+    }
+
+    return getTypeDeclaration(p);
+  }
+
+  @Override
+  public CodegenProperty fromProperty(String name, Property p) {
+    RbiCodegenProperty property = (RbiCodegenProperty) super.fromProperty(name, p);
+    property.rbiDataType = getRbiTypeDeclaration(p);
+
+    return property;
   }
 }
